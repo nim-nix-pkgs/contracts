b6127c465324b4ab4ec29373196b52763a5fa4c8
hugosenari <hugosenari@gmail.com>
2022-03-12 04:12:48 -0300

    chore: re index contracts

---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Design by Contract (DbC) library with minimal runtime.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-contracts-main.flake = false;
+  inputs.src-contracts-main.owner = "Udiknedormin";
+  inputs.src-contracts-main.ref   = "refs/heads/main";
+  inputs.src-contracts-main.repo  = "NimContracts";
+  inputs.src-contracts-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-contracts-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-contracts-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
